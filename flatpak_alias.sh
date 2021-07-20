ddb02393ac272113a633bdd2969071b2a70590a9
advaithm <advaith.madhukar@gmail.com>
2021-07-20 18:23:34 +0530

    removed unneeded file

--- flatpak_alias.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-alias flatseal='flatpak run com.github.tchx84.Flatseal &'
-alias wps='flatpak run com.wps.Office &'
-alias gitahead='flatpak run io.github.gitahead.GitAhead &'
-alias start-github-desktop='flatpak run io.github.shiftey.Desktop &'
-alias flatpak-external-data-checker='flatpak run org.flathub.flatpak-external-data-checker &'
-alias zoom='flatpak run us.zoom.Zoom &'
